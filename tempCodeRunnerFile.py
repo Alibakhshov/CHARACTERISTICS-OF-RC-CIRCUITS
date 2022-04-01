d8c61573f4c534cdd9484aae4c3c83407789e5a4
Rauf Alibakhshov <rauf.alibakhshov_2025@ucentralasia.org>
2022-04-01 22:47:52 +0500

    This commit  solves the two problems given in a lab

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+
+# Import libraries
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+# Creating vectors X and Y
+# t = np.array([205,210,215,220,240,250,260,280,300,300,320,350,400,450,500])
+t = np.array([5,20,50,100,150, 200, 205, 210, 215, 220, 240, 260, 320, 400, 500])
+x= np.array([0,5,10,15,20,25,30,40,50,60,80,100,120,150,200])
+y = 5.357*(1-(2.178)**(-x/21.21))
+z = 5.3566 * 2.178**(-t/21.21)
+ 
+fig = plt.figure(figsize = (10, 5))
+# Create the plot
+plt.plot(x, y)
+plt.plot(t, z)
+ 
+# Show the plot
+plt.show()
